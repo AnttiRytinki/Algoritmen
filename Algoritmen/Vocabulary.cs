0fc0ccf375607f55185d6d8101a3e3b2aa82c7ea
AnttiRytinki <47520749+AnttiRytinki@users.noreply.github.com>
2024-06-05 15:17:05 +0200

    Update Vocabulary.cs

--- Algoritmen/Vocabulary.cs
+++ Algoritmen/Vocabulary.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,34 +9,6 @@ namespace Algoritmen
     public class Vocabulary
     {
         List<string> _strings = new List<string>();
-
-        public int NumberOfStrings => _strings.Count;
-        public string Random => _strings[new Random().Next(_strings.Count)];
-
-        public int LongestStringLength
-        { 
-            get
-            {
-                string s = string.Empty;
-
-                for (int i = 0; i < NumberOfStrings; i++)
-                {
-                    if(_strings[i].Length > s.Length)
-                        s = _strings[i];
-                }
-
-                return s.Length;
-            }
-        }
-
-        public List<string> GetNUniqueRandom(int n)
-        {
-            Random random = new Random();
-            List<string> shuffledStrings = new List<string>(_strings);
-            shuffledStrings.Shuffle(random);
-
-            return shuffledStrings.Take(n).ToList();
-        }
 
         public string Process(string stringToBeProcessed)
         {
